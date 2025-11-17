488095fa90b3f7ae9ca8ad9ad524c59be718276c
mateo cespedes <mateocespedes123@gmail.com>
2025-11-17 19:26:28 -0300

    agregue categorias y productos desde la api

--- js/index.js
+++ js/index.js
@@ -16,3 +16,99 @@ let validarBusqueda = document.querySelector(".buscador");
 
     return true;
 });
+
+
+// carga de categorias (api)
+
+let listaCategorias = document.querySelector("#listaCategorias");
+
+if (listaCategorias) {
+
+    
+    fetch("https://dummyjson.com/products/category-list")
+
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            
+            listaCategorias.innerHTML = "";
+
+            for (let i = 0; i < data.length; i++) {
+                let categoria = data[i];
+
+                
+                listaCategorias.innerHTML += `<li><a href="./product.html?categoria=${categoria}">${categoria}</a></li>`;
+            }
+        })
+        .catch(function(error) {
+            console.log("Error al traer categorías: ", error);
+            listaCategorias.innerHTML = "<li>No se pudieron cargar las categorías.</li>";
+        });
+}
+
+
+// carga de productos (api)
+
+let category1 = document.querySelector("#productosCategoria1");
+let products1 = "";
+
+if (category1) {
+
+    fetch('https://dummyjson.com/products/category/smartphones')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+
+            for(let i = 0; i<10; i++){
+                let producto1 = data.products[i];
+
+                products1 += `<article class="tarjeta-producto">
+                                <img src="${producto1.images[0]}" alt="${producto1.title}">
+                                <h2>${producto1.title}</h2>
+                                <p>${producto1.description}</p>
+                                <p>$ ${producto1.price}</p>
+                                <div><a href="./product.html?id=${producto1.id}">Ver detalle</a></div>
+                              </article>`;
+            }
+
+            category1.innerHTML = products1;
+        })
+        .catch(function(error){
+            console.log("Error al traer productos de la primera sección: " + error);
+            category1.innerHTML = "<p>No se pudieron cargar los productos.</p>";
+        });
+}
+
+let category2 = document.querySelector("#productosCategoria2");
+let products2 = "";
+
+if (category2) {
+
+    fetch('https://dummyjson.com/products/category/mens-shirts')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+
+            for (let i = 0; i < 10; i++) {
+                let producto2 = data.products[i];
+
+                products2 += `<article class="tarjeta-producto">
+                                <img src="${producto2.images[0]}" alt="${producto2.title}">
+                                <h2>${producto2.title}</h2>
+                                <p>${producto2.description}</p>
+                                <p>$ ${producto2.price}</p>
+                                <div><a href="./product.html?id=${producto2.id}">Ver detalle</a></div>
+                              </article>`;
+            }
+
+            category2.innerHTML = products2;
+        })
+        .catch(function(error){
+            console.log("Error al traer productos de la segunda sección: " + error);
+            category2.innerHTML = "<p>No se pudieron cargar los productos.</p>";
+        });
+}
