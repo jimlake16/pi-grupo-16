57a338a4aedb2934b45008a00d9e206de0550869
martin-decillo <decillomartin@gmail.com>
2025-11-20 18:57:22 -0300

    termine todo category

--- js/category.js
+++ js/category.js
@@ -15,6 +15,36 @@ titulo.innerText = category;
 
 
 
+
 // carga de productos (api)
 
+let categ = document.querySelector("#productosCategoria");
+let produc = "";
 
+if (categ) {
+    
+    fetch(`https://dummyjson.com/products/category/${category}`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+
+            for(let i = 0; i< data.products.length; i++){
+                let producto1 = data.products[i];
+
+                produc += `<article>
+                                <img src="${producto1.images[0]}" alt="${producto1.title}">
+                                <h2>${producto1.title}</h2>
+                                <p>${producto1.description}</p>
+                                <p>$ ${producto1.price}</p>
+                                <div><a href="./product.html?id=${producto1.id}">Ver detalle</a></div>
+                              </article>`;
+            }
+
+            categ.innerHTML = produc;
+        })
+        .catch(function(error){
+            console.log("Error: " + error);
+            categ.innerHTML = "<p>No se cargaron lso productos.</p>";
+        });
+}
